0ed9de7a629b9c2b08c0beb83e940272a91ed88a
learn-cloud <51190838+learn-cloud@users.noreply.github.com>
2020-11-15 21:14:50 +0530

    Create packer_install.sh

---
+++ packer_install.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+sudo yum update -y
+
+sudo yum install wget zip unzip -y
+
+sudo mkdir -p /opt/packer
+
+cd /opt/packer
+
+sudo curl -O https://releases.hashicorp.com/packer/1.6.0/packer_1.6.0_linux_amd64.zip
+
+sudo unzip packer_1.6.0_linux_amd64.zip
+
+sudo echo "export PATH=/opt/packer/:$PATH" >> ~/.bash_profile
+
+sudo source ~/.bash_profile
