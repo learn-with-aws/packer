65ecfa81bfba735902a192cd4dfa60e006049e5a
learn-cloud <githubanand@gmail.com>
2020-11-15 15:48:37 +0530

    test

--- nginx/main_script.sh
+++ nginx/main_script.sh
@@ -16,7 +16,7 @@ sudo sh /opt/packer/nginx/scripts/pre-requirements.sh
 sudo sh /opt/packer/nginx/scripts/nginx_install.sh
 
 # Copy the backUp script to Nginx default Path
-sudo cp -pr /opt/packer/nginx/scripts/nginx_logs_backup.sh /etc/nginx/nginx_logs_backup.sh
+sudo cp -pr /opt/packer/nginx/scripts/backup_logs.sh /etc/nginx/backup_logs.sh
 
 # SetUp the CronJob for regular backup
-sudo echo "30 3 * * 0 sh /etc/nginx/nginx_logs_backup.sh" >> /var/spool/cron/root
+sudo echo "30 3 * * 0 sh /etc/nginx/backup_logs.sh" >> /var/spool/cron/root
